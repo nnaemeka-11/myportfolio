d222efedb4db6b0230d0160295c9e533b74158f4
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2024-05-07 10:23:55 +0100

    fixed

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -18,7 +18,7 @@ function Portfolio() {
             <h1 className={styles.Title}>Portfolio</h1>
           </aside>
             <section className={styles.WorkSection}>
-            <Design title={"Smartlearning"} source={smartlearning} text={'https://smartlearning.ng'} classname={styles.Design}/>
+            <Design title={"Smartlearning"} source={smartlearning} text={'https://smartlearning.vercel.app/'} classname={styles.Design}/>
             <Design title={"Card Payment Auth"} source={card} text={'https://junetechepayment.netlify.app/'} classname={styles.Design}/>
             <Design title={"Link Tree Portfolio"} source={linktree} text={'https://nnaemeka11.netlify.app'} classname={styles.Design}/>
             <Design title={"D'Angelo Arts"} source={dangelo} text={'https://nnaemekaxi.github.io'} classname={styles.Design}/>
